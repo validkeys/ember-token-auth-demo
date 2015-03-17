f66783c74049261cfa9f6763ca97bd6411fecfe9
Kyle Davis <validkeys@gmail.com>
2015-03-16 20:58:51 -0400

    Adding in routes and templates on the front end

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -5,6 +5,11 @@ import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+Ember.RSVP.configure('onerror', function(e) {
+  console.log(e.message); 
+  console.log(e.stack);
+});
+
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
