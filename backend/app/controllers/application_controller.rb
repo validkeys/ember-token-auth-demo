a333d2387a7ec5413bb0aa782c0d49604309ea15
Kyle Davis <validkeys@gmail.com>
2015-03-16 20:25:19 -0400

    cleaned up the application and sessions controllers (backend)

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -6,14 +6,38 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+
+  def decoded_token
+    JWT.decode(http_auth_header, Rails.configuration.secret_token)    
+  end
+  hide_action :decoded_token
+
+  def set_current_user(user)
+    @current_user ||= user
+  end
+  hide_action :set_current_user
+
+  private
+
+  def http_auth_header
+    if request.headers['Authorization'].present?
+      request.headers['Authorization'].split(' ').last
+    else
+      nil
+    end
+  end
+
+  def set_current_user_from_token(token)
+    set_current_user User.find(token[0]["user"]["id"])
+  end
+
   def authenticate!
     begin
-      token = request.headers['Authorization'].split(' ').last
-      JWT.decode(token, Rails.configuration.secret_token)
+      set_current_user_from_token(decoded_token)
     rescue JWT::ExpiredSignature
-      puts "Signature has expired"
+      render json: {error: "JWT Expired"}, status: 500
     rescue JWT::DecodeError
-      render json: {error: "There was a problem decoding your JWT"}, status: 500
+      render json: {error: "JWT Decode Error"}, status: 500
     end
   end
 end
--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -5,10 +5,9 @@ class SessionsController < ApplicationController
   def token
     user = User.authenticate(params[:email], params[:password])
     if user
-      payload = { user: user.jwt_params }
-      render json: { 
-        token: JWT.encode(payload, Rails.configuration.secret_token)
-      }, status: 200
+      set_current_user user
+      generated_token = generate_token(user)
+      render json: { token: generated_token[:token], exp: generated_token[:payload][:exp] }, status: 200
     else
       render json: {errors: ["Unauthorized"]}, status: 401
     end
@@ -17,6 +16,16 @@ class SessionsController < ApplicationController
   def token_refresh
     logger.debug("/token_refresh reached")
     render json: {msg: "Unauthorized"}, status: 401
+  end
+
+  private
+
+  def generate_token(user)
+    payload = { user: user.jwt_params, exp: 30.days.from_now.to_i }
+    {
+      token:    JWT.encode(payload, Rails.configuration.secret_token),
+      payload:  payload
+    }
   end
 
 end
